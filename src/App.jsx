38a730403bd1073eb8062dc468aab95d43be086b
vnhan28 <vnhan2808@gmail.com>
2025-12-15 15:37:43 +0700

    them Lazy load component de toi uu performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Layout from "./components/layout/Layout";
-import Login from "./pages/login/Login";
-import Signup from "./pages/login/Signup";
 import Loader from "./components/common/Loader";
-import { selectAuthUser,selectIsCheckingAuth } from "./store/auth/auth.selector";
-import routes from './components/layout/Routes';
+const Login = lazy(() => import("./pages/login/Login"));
+const Signup = lazy(() => import("./pages/login/Signup"));
+
+import {
+  selectAuthUser,
+  selectIsCheckingAuth,
+} from "./store/auth/auth.selector";
+
+import routes from "./components/layout/Routes";
 import { checkAuth } from "./store/auth/auth.thunk";
 import "./styles/style.min.css";
 
 const App = () => {
   const dispatch = useDispatch();
-  const authUser = useSelector(selectAuthUser)
-  const isCheckingAuth = useSelector(selectIsCheckingAuth)
+  const authUser = useSelector(selectAuthUser);
+  const isCheckingAuth = useSelector(selectIsCheckingAuth);
 
   useEffect(() => {
     dispatch(checkAuth());
   }, [dispatch]);
 
+  // đang check auth → loader riêng
   if (isCheckingAuth) return <Loader />;
 
-  //Chưa login → chỉ cho vào login / signup
+  //chưa login
   if (!authUser) {
     return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Suspense>
     );
   }
 
-  // Đã login → toàn bộ app
+  
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Route>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route element={<Layout />}>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
 
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Suspense>
   );
 };
 
--- src/components/layout/Routes.jsx
+++ src/components/layout/Routes.jsx
@@ -1,236 +1,121 @@
-// General
-import NotFound from "../../pages/error/NotFound";
-import Dashboard from "../../pages/dashboard/Overview";
+import { lazy } from "react";
+
+const NotFound = lazy(() => import("../../pages/error/NotFound"));
+const Dashboard = lazy(() => import("../../pages/dashboard/Overview"));
 
 // Media
-import Media from "../../pages/media/Media.jsx";
+const Media = lazy(() => import("../../pages/media/Media.jsx"));
 
 // Settings
-import Api from "../../pages/settings/Api";
-import Email from "../../pages/settings/Email";
-import General from "../../pages/settings/General";
-import CronJob from "../../pages/settings/CronJob";
-import Permalink from "../../pages/settings/Permalink";
-import Languages from "../../pages/settings/Languages";
-import SocialLogin from "../../pages/settings/SocialLogin";
+const Api = lazy(() => import("../../pages/settings/Api"));
+const Email = lazy(() => import("../../pages/settings/Email"));
+const General = lazy(() => import("../../pages/settings/General"));
+const CronJob = lazy(() => import("../../pages/settings/CronJob"));
+const Permalink = lazy(() => import("../../pages/settings/Permalink"));
+const Languages = lazy(() => import("../../pages/settings/Languages"));
+const SocialLogin = lazy(() => import("../../pages/settings/SocialLogin"));
 
 // Products
-import Attribute from "../../pages/products/Attribute";
-import AddProduct from "../../pages/products/AddProduct";
-import EditProduct from "../../pages/products/EditProduct";
-import ManageProduct from "../../pages/products/ManageProduct";
+const Attribute = lazy(() => import("../../pages/products/Attribute"));
+const AddProduct = lazy(() => import("../../pages/products/AddProduct"));
+const EditProduct = lazy(() => import("../../pages/products/EditProduct"));
+const ManageProduct = lazy(() => import("../../pages/products/ManageProduct"));
 
 // Orders
-import AddOrder from "../../pages/orders/AddOrder";
-import ManageOrder from "../../pages/orders/ManageOrder";
-import OrderDetail from "../../pages/orders/OrderDetail";
+const AddOrder = lazy(() => import("../../pages/orders/AddOrder"));
+const ManageOrder = lazy(() => import("../../pages/orders/ManageOrder"));
+const OrderDetail = lazy(() => import("../../pages/orders/OrderDetail"));
 
-// brand
-import AddBrand from "../../pages/brands/AddBrand";
-import ManageBrand from "../../pages/brands/ManageBrand";
-import EditBrand from "../../pages/brands/EditBrand";
+// Brand
+const AddBrand = lazy(() => import("../../pages/brands/AddBrand"));
+const ManageBrand = lazy(() => import("../../pages/brands/ManageBrand"));
+const EditBrand = lazy(() => import("../../pages/brands/EditBrand"));
 
-// Customer
-import AddCustomer from "../../pages/customers/AddCustomer";
-import EditCustomer from "../../pages/customers/EditCustomer";
-import ManageCustomer from "../../pages/customers/ManageCustomer";
+// Customers
+const AddCustomer = lazy(() => import("../../pages/customers/AddCustomer"));
+const EditCustomer = lazy(() => import("../../pages/customers/EditCustomer"));
+const ManageCustomer = lazy(() => import("../../pages/customers/ManageCustomer"));
 
 // Users
-import AddUser from "../../pages/users/AddUser";
-import EditUser from "../../pages/users/EditUser";
-import UserList from "../../pages/users/UserList";
+const AddUser = lazy(() => import("../../pages/users/AddUser"));
+const EditUser = lazy(() => import("../../pages/users/EditUser"));
+const UserList = lazy(() => import("../../pages/users/UserList"));
 
 // Venue
-import AddVenue from "../../pages/venue/AddVenue";
-import ManageVenue from "../../pages/venue/ManageVenue";
+const AddVenue = lazy(() => import("../../pages/venue/AddVenue"));
+const ManageVenue = lazy(() => import("../../pages/venue/ManageVenue"));
 
 // Categories
-import AddCategories from "../../pages/categories/AddCategories";
-import EditCategories from "../../pages/categories/EditCategories";
-import ManageCategories from "../../pages/categories/ManageCategories";
+const AddCategories = lazy(() => import("../../pages/categories/AddCategories"));
+const EditCategories = lazy(() => import("../../pages/categories/EditCategories"));
+const ManageCategories = lazy(() => import("../../pages/categories/ManageCategories"));
 
 // Reviews
-import ManageReviews from "../../pages/reviews/ManageReviews";
-import ReviewsDetail from "../../pages/reviews/ReviewsDetail";
+const ManageReviews = lazy(() => import("../../pages/reviews/ManageReviews"));
+const ReviewsDetail = lazy(() => import("../../pages/reviews/ReviewsDetail"));
 
 // Pages
-import AddPage from "../../pages/pages/AddPage";
-import EditPage from "../../pages/pages/EditPage";
-import ManagePages from "../../pages/pages/ManagePages";
+const AddPage = lazy(() => import("../../pages/pages/AddPage"));
+const EditPage = lazy(() => import("../../pages/pages/EditPage"));
+const ManagePages = lazy(() => import("../../pages/pages/ManagePages"));
 
 // Payment
-import ManageTransactions from "../../pages/payment/ManageTransactions";
-import PaymentMethod from "../../pages/payment/PaymentMethod";
-import TransactionDetail from "../../pages/payment/TransactionDetail";
-
-// Sorting and Comments
+const ManageTransactions = lazy(() => import("../../pages/payment/ManageTransactions"));
+const PaymentMethod = lazy(() => import("../../pages/payment/PaymentMethod"));
+const TransactionDetail = lazy(() => import("../../pages/payment/TransactionDetail"));
 
 const routes = [
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  // Catalog
-  {
-    path: "/catalog/product/add",
-    element: <AddProduct />,
-  },
-  {
-    path: "/catalog/product/manage",
-    element: <ManageProduct />,
-  },
-  {
-    path: "/catalog/product/manage/:productId",
-    element: <EditProduct />,
-  },
-  {
-    path: "/catalog/product/attribute",
-    element: <Attribute />,
-  },
-  // orders
-  {
-    path: "/orders/add",
-    element: <AddOrder />,
-  },
-  {
-    path: "/orders/manage",
-    element: <ManageOrder />,
-  },
-  {
-    path: "/orders/manage/:orderID",
-    element: <OrderDetail />,
-  },
-  // Catalog Categories
-  {
-    path: "/catalog/categories/manage",
-    element: <ManageCategories />,
-  },
-  {
-    path: "/catalog/categories/:categoryid",
-    element: <EditCategories />,
-  },
-  // customers
-  {
-    path: "/customers/add",
-    element: <AddCustomer />,
-  },
-  {
-    path: "/customers/manage",
-    element: <ManageCustomer />,
-  },
-  {
-    path: "/customers/manage/:customerId",
-    element: <EditCustomer />,
-  },
-  // brand
-  {
-    path: "/brands/add",
-    element: <AddBrand />,
-  },
-  {
-    path: "/brands/manage",
-    element: <ManageBrand />,
-  },
-  {
-    path: "/brands/manage/:brandId",
-    element: <EditBrand />,
-  },
-  // Users
-  {
-    path: "/users/list",
-    element: <UserList />,
-  },
-  {
-    path: "/users/add",
-    element: <AddUser />,
-  },
-  {
-    path: "/users/list/:userid",
-    element: <EditUser />,
-  },
-  // Venue
-  {
-    path: "/venue/add",
-    element: <AddVenue />,
-  },
-  {
-    path: "/venue/manage",
-    element: <ManageVenue />,
-  },
-  // Reviews
-  {
-    path: "/reviews",
-    element: <ManageReviews />,
-  },
-  {
-    path: "/reviews/:reviewid",
-    element: <ReviewsDetail />,
-  },
-  // Pages
-  {
-    path: "/pages",
-    element: <ManagePages />,
-  },
-  {
-    path: "/pages/add",
-    element: <AddPage />,
-  },
-  {
-    path: "/pages/:pageId",
-    element: <EditPage />,
-  },
-  // Payment
-  {
-    path: "/payment/transactions",
-    element: <ManageTransactions />,
-  },
-  {
-    path: "/payment/transactions/:transactionId",
-    element: <TransactionDetail />,
-  },
-  {
-    path: "/payment/payment-method",
-    element: <PaymentMethod />,
-  },
-  // Media
-  {
-    path: "/media",
-    element: <Media />,
-  },
-  // Settings
-  {
-    path: "/setting/general",
-    element: <General />,
-  },
-  {
-    path: "/setting/email",
-    element: <Email />,
-  },
-  {
-    path: "/setting/cronJob",
-    element: <CronJob />,
-  },
-  {
-    path: "/setting/permalink",
-    element: <Permalink />,
-  },
-  {
-    path: "/setting/languages",
-    element: <Languages />,
-  },
-  {
-    path: "/setting/social-login",
-    element: <SocialLogin />,
-  },
-  {
-    path: "/setting/api",
-    element: <Api />,
-  },
-  // Not Found
-  {
-    path: "*",
-    element: <NotFound />,
-  },
+  { path: "/", element: <Dashboard /> },
+
+  { path: "/catalog/product/add", element: <AddProduct /> },
+  { path: "/catalog/product/manage", element: <ManageProduct /> },
+  { path: "/catalog/product/manage/:productId", element: <EditProduct /> },
+  { path: "/catalog/product/attribute", element: <Attribute /> },
+
+  { path: "/orders/add", element: <AddOrder /> },
+  { path: "/orders/manage", element: <ManageOrder /> },
+  { path: "/orders/manage/:orderID", element: <OrderDetail /> },
+
+  { path: "/catalog/categories/manage", element: <ManageCategories /> },
+  { path: "/catalog/categories/:categoryid", element: <EditCategories /> },
+
+  { path: "/customers/add", element: <AddCustomer /> },
+  { path: "/customers/manage", element: <ManageCustomer /> },
+  { path: "/customers/manage/:customerId", element: <EditCustomer /> },
+
+  { path: "/brands/add", element: <AddBrand /> },
+  { path: "/brands/manage", element: <ManageBrand /> },
+  { path: "/brands/manage/:brandId", element: <EditBrand /> },
+
+  { path: "/users/list", element: <UserList /> },
+  { path: "/users/add", element: <AddUser /> },
+  { path: "/users/list/:userid", element: <EditUser /> },
+
+  { path: "/venue/add", element: <AddVenue /> },
+  { path: "/venue/manage", element: <ManageVenue /> },
+
+  { path: "/reviews", element: <ManageReviews /> },
+  { path: "/reviews/:reviewid", element: <ReviewsDetail /> },
+
+  { path: "/pages", element: <ManagePages /> },
+  { path: "/pages/add", element: <AddPage /> },
+  { path: "/pages/:pageId", element: <EditPage /> },
+
+  { path: "/payment/transactions", element: <ManageTransactions /> },
+  { path: "/payment/transactions/:transactionId", element: <TransactionDetail /> },
+  { path: "/payment/payment-method", element: <PaymentMethod /> },
+
+  { path: "/media", element: <Media /> },
+
+  { path: "/setting/general", element: <General /> },
+  { path: "/setting/email", element: <Email /> },
+  { path: "/setting/cronJob", element: <CronJob /> },
+  { path: "/setting/permalink", element: <Permalink /> },
+  { path: "/setting/languages", element: <Languages /> },
+  { path: "/setting/social-login", element: <SocialLogin /> },
+  { path: "/setting/api", element: <Api /> },
+
+  { path: "*", element: <NotFound /> },
 ];
 
 export default routes;
